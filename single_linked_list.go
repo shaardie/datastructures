f7f000bc3a775751c9746e271f8267359c53ab5c
Sven Haardiek <sven@haardiek.de>
2019-09-21 12:32:09 +0200

    Switch PC
    Signed-off-by: Sven Haardiek <sven@haardiek.de>

--- single_linked_list.go
+++ single_linked_list.go
@@ -40,74 +40,81 @@ func (sll *SingleLinkedList) Length() int {
 	return sll.length
 }
 
-func (sll *SingleLinkedList) Append(element interface{}) {
-	newNode := node{
-		value: element,
-		next:  nil,
+func (sll *SingleLinkedList) Insert(index int, element interface{}) {
+
+	switch {
+	case index < 0:
+	case index == 0:
 	}
 
-	if sll.first == nil {
-		sll.first = &newNode
+	if index < 0 {
+		panic("index < 0")
+	}
+
+	if index == 0 {
+		sll.first = &node{value: element, next: sll.first.next}
 		sll.length++
 		return
 	}
 
-	n := sll.first
-	for {
-		if n.next == nil {
-			n.next = &node{
-				value: element,
-				next:  nil,
-			}
-			sll.length++
-			return
-		}
-		n = n.next
-	}
-}
-
-func (sll *SingleLinkedList) Prepend(element interface{}) {
-	sll.first = &node{
-		value: element,
-		next:  sll.first,
-	}
+	n := sll.getNode(index - 1)
+	sll.insertAfter(&node{value: element}, n)
 	sll.length++
 }
 
+func (sll *SingleLinkedList) Append(element interface{}) {
+	sll.Insert(sll.Length(), element)
+}
+
+func (sll *SingleLinkedList) Prepend(element interface{}) {
+	sll.Insert(0, element)
+}
+
 func (sll *SingleLinkedList) Get(index int) interface{} {
-	n := sll.first
-	for index > 0 {
-		n = n.next
-		index--
-	}
-	return n.value
+	return sll.getNode(index).value
 }
 
 func (sll *SingleLinkedList) Pop(index int) interface{} {
 
-	if index == 0 {
+	if index < 0 {
+		panic("index < 0")
+	} else if index == 0 {
 		v := sll.first.value
 		sll.first = sll.first.next
 		sll.length--
 		return v
 	}
 
-	before := sll.first
-	n := sll.first.next
+	n := sll.first
 	index--
-
-	for index < 0 {
-		before = n
+	for index != 0 {
 		n = n.next
 		index--
 	}
 
-	before.next = n.next
+	v := n.next.value
+	n.next = n.next.next
 	sll.length--
 
-	return n.value
+	return v
 }
 
 func (sll *SingleLinkedList) Delete(index int) {
 	sll.Pop(index)
+}
+
+func (sll *SingleLinkedList) insertAfter(first *node, after *node) {
+	if after != nil {
+		after.next = first.next
+	}
+	first.next = after
+}
+
+func (sll *SingleLinkedList) getNode(index int) *node {
+	n := sll.first
+	for index > 0 {
+		n = n.next
+		index--
+	}
+	return n
 }
--- single_linked_list_test.go
+++ single_linked_list_test.go
@@ -8,27 +8,33 @@ import (
 )
 
 func TestSingleLinkedList(t *testing.T) {
-
 	list := []int{
 		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 	}
-
 	sll := NewSingleLinkedList()
-	l := 1
-	for _, element := range list {
+	for index, element := range list {
 		sll.Append(element)
-		assert.Equal(t, l, sll.Length(), "Length wrong")
-		l++
+		assert.Equal(t, index+1, sll.Length(), "Length wrong")
+		t.Log(sll)
 	}
 	assert.Equal(t, fmt.Sprint(sll), fmt.Sprint(list))
-	for _, element := range list {
+	for index, element := range list {
 		sll.Prepend(element)
-		assert.Equal(t, l, sll.Length(), "Length wrong")
-		l++
+		assert.Equal(t, index+len(list), sll.Length(), "Length wrong")
 	}
 
 	for i := 0; i < len(list); i++ {
 		assert.Equal(t, list[i], sll.Pop(10))
-		t.Logf("%v", sll)
 	}
+
+	for i := 0; i < len(list); i++ {
+		assert.Equal(t, list[9-i], sll.Get(i))
+	}
+
+	for i := 0; i < len(list); i++ {
+		sll.Delete(0)
+		assert.Equal(t, 9-i, sll.Length())
+	}
+
+	assert.Panics(t, func() { sll.Pop(-1) })
 }
