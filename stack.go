ab08714119757776471f2df47c5a16644d9c86cc
Sven Haardiek <sven@haardiek.de>
2019-09-22 00:37:53 +0200

    Stack implementation
    Signed-off-by: Sven Haardiek <sven@haardiek.de>

--- stack.go
+++ stack.go
@@ -1,4 +1,19 @@
 package datastructures
 
+// Stack is a basic implementation of a stack.
+//
+// For more information about the data structure take a look at
+// https://en.wikipedia.org/wiki/Stack_(abstract_data_type).Stack
 type Stack struct {
+	sll *SingleLinkedList
+}
+
+// Push a new element onto the Stack.
+func (s Stack) Push(element interface{}) {
+	s.sll.Prepend(element)
+}
+
+// Pop the top element from the Stack.
+func (s Stack) Pop() interface{} {
+	return s.sll.Pop(0)
 }
